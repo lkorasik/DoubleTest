b086b5a94286607f1f9b39e1ffacb041db1ca34c
lev <lkorasik83@gmail.com>
2022-02-10 12:19:58 +0300

    Добавил логгирование activity's lifecycle

--- app/src/main/java/com/example/doubletest/MainActivity.kt
+++ app/src/main/java/com/example/doubletest/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.example.doubletest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        LogExtension.i("onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        LogExtension.i("onStart")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        LogExtension.i("onRestart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        LogExtension.i("onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        LogExtension.i("onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        LogExtension.i("onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        LogExtension.i("onDestroy")
     }
 }
