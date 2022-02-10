a5b6c8807030fac5b2f779c163c5f3aa68d52a28
lev <lkorasik83@gmail.com>
2022-02-10 13:23:18 +0300

    Добавил счетчик и его увеличение при смене конфигурации

--- app/src/main/java/com/example/doubletest/MainActivity.kt
+++ app/src/main/java/com/example/doubletest/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.doubletest
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var counterView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        counterView = findViewById(R.id.counter)
+        counterView.text = 0.toString()
 
         LogExtension.i("onCreate")
     }
@@ -45,5 +51,25 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
 
         LogExtension.i("onDestroy")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putInt(COUNTER_KEY, counterView.text.toString().toInt() + 1)
+
+        LogExtension.i("onSaveInstanceState")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        counterView.text = savedInstanceState.getInt(COUNTER_KEY).toString()
+
+        LogExtension.i("onRestoreInstanceState")
+    }
+
+    companion object{
+        const val COUNTER_KEY = "COUNTER"
     }
 }
